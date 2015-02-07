44fa4ea42704e3e0a800fda28e892bf65c6f7f21
Wen <ws@wenshao.ws>
2015-02-07 15:48:28 +1100

    Create assignment 1

---
+++ assignment1/assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Wen Shao on 7/02/2015.
+//  Copyright (c) 2015 Wen Shao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
